38ad0b4df806fd86e86fc8fd458c98bbcb46f69d
xinton <washingtonbruno1@gmail.com>
2022-07-25 20:39:06 -0300

    login action and informations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def authorize
+    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def organization_name
+    @organization_name ? Organization.find_by_id(current_user.organization_id).name : nil
+  end
+  helper_method :current_user, :organization_name
 end
