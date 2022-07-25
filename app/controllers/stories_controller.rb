6753e2637af8616941818785479ae4b1771fb34a
xinton <washingtonbruno1@gmail.com>
2022-07-25 20:54:12 -0300

    add list stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,7 @@
 class StoriesController < ApplicationController
+  before_action :authorize
+  
+  def index
+    @stories = Story.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :stories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "stories#index"
   
+  resources :stories
+
   resources :sessions
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
