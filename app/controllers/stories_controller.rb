fe8d2693217ef1bcb8cea3fd47ebb0fd62dec294
xinton <washingtonbruno1@gmail.com>
2022-07-25 21:10:30 -0300

    create, update and destroy actions

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,4 +4,50 @@ class StoriesController < ApplicationController
   def index
     @stories = Story.all
   end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(article_params)
+    @story.organization = Organization.find_by_id(1)
+    @story.chief = User.find_by_id(1)
+
+    if @story.save
+      redirect_to stories_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:id])
+
+    if @story.update(article_params)
+      redirect_to @story
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+  
+  private
+    def article_params
+      params.require(:story).permit(:headline, :body, :chief, :organization)
+    end
 end
