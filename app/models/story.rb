86d8db5afc7b14d2053397e301f24b6c68c1fad3
xinton <washingtonbruno1@gmail.com>
2022-07-25 21:05:41 -0300

    fix associoations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ApplicationRecord
-  belongs_to :chief
-  belongs_to :writer
-  belongs_to :reviewer
+  belongs_to :chief, :class_name => "User", foreign_key: :chief_id
+  belongs_to :writer, :class_name => "User", foreign_key: :writer_id, optional: true
+  belongs_to :reviewer, :class_name => "User", foreign_key: :reviewer_id, optional: true
   belongs_to :organization
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :chiefing_stories, :class_name => 'Story', :foreign_key => 'chief_id'
+  has_many :write_stories, :class_name => 'Story', :foreign_key => 'writer_id'
+  has_many :revised_stories, :class_name => 'Story', :foreign_key => 'reviewer_id'
+  
   belongs_to :organization
 
   validates_uniqueness_of :email
