b19d74a257b48f2713eb75b1c9619c995750750c
xinton <washingtonbruno1@gmail.com>
2022-07-27 09:49:46 -0300

    WIP add some  validations

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -17,8 +17,7 @@ class StoriesController < ApplicationController
     @story = Story.new(article_params)
     @story.organization = current_user.organization
     @story.chief = current_user
-
-    writer_on_creation
+    @story.writer = article_params[:writer_id].present? ? User.find(article_params[:writer_id]) : nil
     @story.reviewer = article_params[:reviewer_id].present? ? User.find(article_params[:reviewer_id]) : nil
 
     if @story.save
@@ -34,6 +33,11 @@ class StoriesController < ApplicationController
 
   def update
     @story = Story.find(params[:id])
+
+    @story.writer = article_params[:writer_id].present? ? User.find(article_params[:writer_id]) : nil
+    @story.reviewer = article_params[:reviewer_id].present? ? User.find(article_params[:reviewer_id]) : nil
+    
+    handle_state
 
     if @story.update(article_params)
       redirect_to @story
@@ -51,15 +55,29 @@ class StoriesController < ApplicationController
   
   private
     def article_params
-      params.require(:story).permit(:headline, :body, :chief, :organization, :writer_id, :reviewer_id)
+      params.require(:story).permit(:headline, :body, :status, :chief, :organization, :writer, :writer_id, :reviewer, :reviewer_id)
     end
 
-    def writer_on_creation
-      if ( article_params[:writer_id].present? )
-        @story.writer =  User.find(article_params[:writer_id])
-        @story.create_with_writer
+    def current_action_text
+      case @story.status
+      when 'draft'
+        "REQUEST REVIEW"
+      
       else
-        @story.create_without_writer
+        "Save"
       end
     end
+
+    def handle_state
+      case @story.status
+      when 'unassigned'
+        @story.assign_writer
+      when 'draft'
+        @story.request_review      
+      else
+        "Error"
+      end
+    end
+
+    helper_method :current_action_text, :current_action
 end
--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -10,4 +10,8 @@ module StoriesHelper
   def reviewer?
     current_user.role == "reviewer"
   end
+
+  def can_write?
+    writer? && current_user.id == @story.writer.id
+  end
 end
