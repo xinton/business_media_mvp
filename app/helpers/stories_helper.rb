c33ba6a0b31458208943bc88c69bb3df433b9c61
xinton <washingtonbruno1@gmail.com>
2022-07-26 21:20:20 -0300

    verify roler helper

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,13 @@
 module StoriesHelper
+  def chief?
+    current_user.role == "chief"
+  end
+
+  def writer?
+    current_user.role == "writer"
+  end
+
+  def reviewer?
+    current_user.role == "reviewer"
+  end
 end
