34ffa9c806eec1aff620cdaf947efdc04b2611ab
xinton <washingtonbruno1@gmail.com>
2022-07-27 14:33:36 -0300

    WIP added basic flow

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,6 @@
 class StoriesController < ApplicationController
+  include StoriesHelper
+
   before_action :authorize
   
   def index
@@ -17,8 +19,9 @@ class StoriesController < ApplicationController
     @story = Story.new(article_params)
     @story.organization = current_user.organization
     @story.chief = current_user
-    @story.writer = article_params[:writer_id].present? ? User.find(article_params[:writer_id]) : nil
     @story.reviewer = article_params[:reviewer_id].present? ? User.find(article_params[:reviewer_id]) : nil
+    
+    handle_writer_on_creation
 
     if @story.save
       redirect_to stories_path
@@ -37,6 +40,7 @@ class StoriesController < ApplicationController
     @story.writer = article_params[:writer_id].present? ? User.find(article_params[:writer_id]) : nil
     @story.reviewer = article_params[:reviewer_id].present? ? User.find(article_params[:reviewer_id]) : nil
     
+    # state machine
     handle_state
 
     if @story.update(article_params)
@@ -58,11 +62,45 @@ class StoriesController < ApplicationController
       params.require(:story).permit(:headline, :body, :status, :chief, :organization, :writer, :writer_id, :reviewer, :reviewer_id)
     end
 
+    def handle_writer_on_creation
+      if article_params[:writer_id].present?
+        @story.writer = User.find(article_params[:writer_id])
+        @story.create_with_writer
+      else
+        @story.create_without_writer
+      end
+    end
+
     def current_action_text
+      #TODO change to hash array
       case @story.status
+      when 'unassigned'
+        "SAVE"
       when 'draft'
         "REQUEST REVIEW"
-      
+      when 'for_review'
+        "REVIEW"
+      when 'in_review'
+        "APPROVE"
+      when 'approved'
+        "PUBLISH"
+      else
+        "Save"
+      end
+    end
+    
+    def current_action_permission
+      reviwer_status = ['for_review', 'in_review', 'pending']
+
+      case @story.status
+      when 'unassigned'
+        "SAVE"
+      when 'draft'
+        story_write?
+      when *reviwer_status
+        story_reviewer?
+      when 'approved'
+        story_chief?
       else
         "Save"
       end
@@ -73,11 +111,17 @@ class StoriesController < ApplicationController
       when 'unassigned'
         @story.assign_writer
       when 'draft'
-        @story.request_review      
+        @story.request_review    
+      when 'for_review'
+        @story.start_review   
+      when 'in_review'
+        @story.approve 
+      when 'approved'
+        @story.publish   
       else
         "Error"
       end
     end
 
-    helper_method :current_action_text, :current_action
+    helper_method :current_action_text, :current_action_permission
 end
--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -3,15 +3,31 @@ module StoriesHelper
     current_user.role == "chief"
   end
 
+  def story_chief?
+    chief? && current_user.id == @story.chief&.id
+  end
+
   def writer?
     current_user.role == "writer"
+  end
+
+  def story_write?
+    writer? && current_user.id == @story.writer&.id
+  end
+
+  def can_write?
+    story_write? && ["draft", "pending"].include?(@story.status)
   end
 
   def reviewer?
     current_user.role == "reviewer"
   end
 
-  def can_write?
-    writer? && current_user.id == @story.writer.id
+  def story_reviewer?
+    reviewer? && current_user.id == @story.reviewer&.id
+  end
+
+  def can_review?
+    story_reviewer? && ["in_review", "for_review"].include?(@story.status)
   end
 end
